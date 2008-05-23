c1e269e4601f7a8626f46d36c2c3ef74d822c544
FPGA <FPGA@8bcac300-4d60-4aec-84ba-b7489f70e69b>
2008-05-23 03:04:49 +0000

    Minimal changes to the license templates.
    It should be "the STI" in the license text, not simply "STI".
    --
    Olaf Mandel
    
    
    git-svn-id: https://atom-svn.stanford.edu/svn/FPGA/sti@84 8bcac300-4d60-4aec-84ba-b7489f70e69b

--- client/template.java
+++ client/template.java
@@ -10,18 +10,18 @@
  *  Copyright (C) 2008 Jason Hogan <hogan@stanford.edu>\n
  *  Copyright (C) 2008 David Johnson <dmsj@stanford.edu>\n
  *  Copyright (C) 2008 Olaf Mandel <mandel@stanford.edu>\n
- *  This file is part of Stanford Timing Interface (STI).
+ *  This file is part of the Stanford Timing Interface (STI).
  *
- *  STI is free software: you can redistribute it and/or modify
+ *  The STI is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  STI is distributed in the hope that it will be useful,
+ *  The STI is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with STI.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with the STI.  If not, see <http://www.gnu.org/licenses/>.
  */
