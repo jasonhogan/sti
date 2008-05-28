989d9a06f9526f3f7078d11560a3f386d6c55e60
FPGA <FPGA@8bcac300-4d60-4aec-84ba-b7489f70e69b>
2008-05-28 20:08:10 +0000

    Reaction to r95.
    * Copied the change of r95 from corba/dummysrv_gnu to sti/server (and clarified
      it in the process).
    * Removed the no longer needed corba/dummysrv_gnu.
    * Found that sti/client.idl should be svn:eol-style = native.
    --
    Olaf Mandel
    
    
    git-svn-id: https://atom-svn.stanford.edu/svn/FPGA/sti@96 8bcac300-4d60-4aec-84ba-b7489f70e69b

--- server/autoinit.sh
+++ server/autoinit.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 #
 # Run this script after downloading only the minimal files stored in the
-# SVN repository.
+# SVN repository. You will need the following packages installed on your
+# system to run this script:
+#   * autoconf
+#   * automake
+#   * autotools-idl (not part of Debian, yet. Stay tuned)
 #
 
 set -e
