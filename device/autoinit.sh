fcd5e1d15ae0e7b82fa1573638cc71810895365a
FPGA <FPGA@8bcac300-4d60-4aec-84ba-b7489f70e69b>
2008-06-04 17:49:26 +0000

    Added GNU build system to STI/device/.
    At the moment, the make-files don't do anything
    
    
    git-svn-id: https://atom-svn.stanford.edu/svn/FPGA/sti@108 8bcac300-4d60-4aec-84ba-b7489f70e69b

--- device/autoinit.sh
+++ device/autoinit.sh
@@ -17,18 +17,12 @@ if test -z "${RELPATH}"; then
 fi
 cd "${RELPATH}"
 
-echo 'Now initializing the client build system. Please wait...'
-client/autoinit.sh >/dev/null
-echo 'Now initializing the device build system. Please wait...'
-device/autoinit.sh >/dev/null
-echo 'Now initializing the server build system. Please wait...'
-server/autoinit.sh >/dev/null
+echo 'Now initializing the build system. Please wait...'
 
-echo 'Now initializing the project build system. Please wait...'
 mkdir -p config
 autoreconf --install --symlink >/dev/null
 
 echo
 echo 'You can now follow the usual path of "./configure && make".'
 echo 'To locally install the software, run "make install" afterwards.'
-echo 'To create the documentation, run "doxygen" in the subdirectories.'
+echo 'To create the documentation, run "doxygen".'
