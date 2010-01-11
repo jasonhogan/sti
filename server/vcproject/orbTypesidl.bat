1907d6c7c4ccda8723111358fdacfd926e276bdd
FPGA <hogan@stanford.edu>
2010-01-11 05:18:58 +0000

    TEMPORARY - Moving sti/trunk back to sti-main/branches/sti
    * Log messages didn't transfer.
    
    Jason

--- server/vcproject/orbTypesidl.bat
+++
@@ -1,8 +0,0 @@
-cls
-
-omniidl -bcxx -C.\..\src\corba -Wbh=.h -Wbs=.cpp .\..\..\idl\orbTypes.idl
-
-copy .\..\src\corba\orbTypes.h .\..\..\device\include
-copy .\..\src\corba\orbTypes.cpp .\..\..\device\src
-
-pause
