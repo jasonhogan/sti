b41d51f3ccce8649fd0cac1f58f13a884cc2c24b
FPGA <FPGA@8bcac300-4d60-4aec-84ba-b7489f70e69b>
2010-01-11 05:18:58 +0000

    TEMPORARY - Moving sti/trunk back to sti-main/branches/sti
    * Log messages didn't transfer.
    
    Jason
    
    git-svn-id: https://atom-svn.stanford.edu/svn/FPGA/sti/trunk@619 8bcac300-4d60-4aec-84ba-b7489f70e69b

--- fileserver/vcproject/FileServeridl.bat
+++
@@ -1,5 +0,0 @@
-cls
-
-omniidl -bcxx -C.\..\src -Wbh=.h -Wbs=.cpp .\..\..\idl\FileServer.idl
-
-pause
--- server/vcproject/clientidl.bat
+++
@@ -1,9 +0,0 @@
-cls
-
-cd ..\..\idl\
-
-omniidl -bcxx -C.\..\server\src\corba -Wbh=.h -Wbs=.cpp .\client.idl
-
-cd ..\server\vcproject\
-
-pause
