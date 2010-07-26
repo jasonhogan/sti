f13a1519fb2d2d77580cafa4ebedba293e6be1ba
FPGA <FPGA@8bcac300-4d60-4aec-84ba-b7489f70e69b>
2010-07-26 22:43:36 +0000

    Updated MCL python file
    -Susannah
    
    git-svn-id: https://atom-svn.stanford.edu/svn/FPGA/sti/trunk@784 8bcac300-4d60-4aec-84ba-b7489f70e69b

--- test/MCL/MCLtest.py
+++ test/MCL/MCLtest.py
@@ -7,71 +7,28 @@ s = 1000000000.0
 
 # Set description used by program
 
-include('channels.py')
-include('motFunction.py')
-include('absorptionImageFunction.py')
+nanoDrive=dev('MCL NanoDrive','ep-timing1.stanford.edu',0)
+mirrorPosition=ch(nanoDrive,0)
+measMirrorPosition=ch(nanoDrive,1)
 
-
-### Andor Camera ###
-andorCamera = dev('Andor iXon 885','ep-timing1.stanford.edu',0)
-camera = ch(andorCamera, 0)
-
-
-
-setvar('imageCropVector',(500,500,480))
-setvar('dtDriftTime', 15*us)
-
-setvar('MOTLoadTime', 400*ms)
-
-
+# defined variables
+setvar('thetaStart', 0)
+setvar('phiStart',0)
+setvar('zStart',0)
+setvar('inc', .1)
+setvar('startPosition',0)
+setvar('endPosition',30)
+setvar('settlingTime', 100*ms)
+setvar('dtSetAxes',300*ms)
 # Global definitions
 
 t0 = 11*us + 2*ms
 
-meas(ch(wavemeter, 0), t0, "Measure 1530 frequency")
-meas(absoptionLightFrequency, t0)
+for i in range(0,endPosition/inc + 1):
+    event(mirrorPosition, t0+i*dtSetAxes, (thetaStart,phiStart,zStart+i*inc))
+    meas(measMirrorPosition, t0+i*dtSetAxes+settlingTime)
 
-event(starkShiftingAOM, 100*us, starkShiftOff)
-event(probeLightAOM, t0, probeLightOff)             # AOM is off, so no imaging light
-
-time = t0
-time = MOT(time, tClearTime=100*ms, cMOT=False, dtMOTLoad=MOTLoadTime)
-motFinishedLoading=time
-
-
-##1530 experiment
-
-setvar('darkSpotOn', False)
-setvar('depumpMOT',True)
-
-
-if(darkSpotOn) :
-    setvar('desc',"Atoms in dark spot--1530 On")
-    event(starkShiftingAOM, t0+50*ms, starkShiftOn)
-    event(starkShiftingAOM, motFinishedLoading, starkShiftOff)
-else :
-    setvar('desc',"Atoms in dark spot--1530 Off")
-    event(starkShiftingAOM, t0+50*ms, starkShiftOff)
-    event(starkShiftingAOM, motFinishedLoading, starkShiftOff)
-
-
-#depump the MOT
-setvar('depumpTime', 2*us)
-
-if(depumpMOT) :
-    event(repumpFrequencySwitch, motFinishedLoading - depumpTime, 1)
-    event(motFrequencySwitch, motFinishedLoading - depumpTime, 1)
-    event(repumpFrequencySwitch, motFinishedLoading, 0)
-    event(motFrequencySwitch, motFinishedLoading, 0)
-
-
-##Image
-dtDeadMOT = 100*ms
-time = takeAbsorptionImage(time + dtDriftTime, time + dtDriftTime + dtDeadMOT, cropVector=imageCropVector)
-
-
-## Turn on MOT steady state
-
-tTAEndOfSequence = time +2*ms
-time = MOT(tTAEndOfSequence, leaveOn=True)    # turn MOT back on
+#set back to zero with a certain buffer
+event(mirrorPosition, t0+(endPosition/inc + 1)*dtSetAxes, (thetaStart,phiStart,zStart))
+event(mirrorPosition, t0+(endPosition/inc + 2)*dtSetAxes, (thetaStart,phiStart,zStart))
 
