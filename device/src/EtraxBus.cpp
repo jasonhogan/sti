32962745d38b9af9674501834188ee63c49fd5e6
FPGA <FPGA@8bcac300-4d60-4aec-84ba-b7489f70e69b>
2012-03-26 00:02:07 +0000

    EtraxBus modification to eliminate excessive bus mapping
    * Attempting to get rid of memory leak during parsing to increase FPGA device stability.
    * FPGA devices should now only call bus_space_map whenever their memory addresses are changed (very rare) instead of every time parse is called.
    
    Jason
    
    
    git-svn-id: https://atom-svn.stanford.edu/svn/FPGA/sti/trunk@934 8bcac300-4d60-4aec-84ba-b7489f70e69b

--- device/src/EtraxBus.cpp
+++ device/src/EtraxBus.cpp
@@ -31,6 +31,10 @@ using namespace std;
 
 EtraxBus::EtraxBus(uInt32 MemoryAddress, uInt32 NumberOfWords)
 {
+	tag = NULL;
+	lastMemoryAddress = 0;
+	lastNumberOfWords = NumberOfWords - 1;	//this way lastNumberOfWords is guaranteed to be different from NumberOfWords;
+
 	setMemoryAddress(MemoryAddress, NumberOfWords);
 
 	readMutex = new omni_mutex();
@@ -43,41 +47,49 @@ EtraxBus::~EtraxBus()
 
 void EtraxBus::setupMemoryBus()
 {
+//#define	HAVE_LIBBUS
 #ifdef HAVE_LIBBUS
+	
 	int ret;
 
 	/* Grab bus space. */
 	
 	if(tag == NULL)
 	{							
-		if ((ret = bus_space_tag(&tag, "memio")) != 0)
-		//	errx(1, "could not access physical memory, error %d", ret);
-		std::cerr << "Could not access physical memory, error " << ret << std::endl;
-
-		if((ret=bus_space_map(tag, 0xb0014008, 0x00000004, 0, &ioh1)) != 0)
+		if( (ret = bus_space_tag(&tag, "memio")) != 0 )	//This function calls memio_tag(tag) (defined in bus_space.c)
+														//which sets bus_space_tag::bst_ops to the 
+														//(static) memio_ops (defined in bus_space.c).
+														//memio_ops sets the map funtion pointer to memio_map (defined in bus_space.c)
 		{
-		std::cerr << "Couldn't map bus space for speed register, error: "
-		  << STI::Utils::valueToString(ret) << std::endl;
+			std::cerr << "Could not access physical memory, error " << ret << std::endl;
+		}
+
+		if( (ret = bus_space_map(tag, 0xb0014008, 0x00000004, 0, &ioh1)) != 0 )
+		{
+			std::cerr << "Couldn't map bus space for speed register, error: "
+				<< STI::Utils::valueToString(ret) << std::endl;
 		}
 
 		//Change the number of wait cycles to 4 (modified 4/29/2009 David Johnson from 2 to 4 - for the modern FPGA wishbone code)
-		old_speed=bus_space_read_4(tag, ioh1, 0);
+		old_speed = bus_space_read_4(tag, ioh1, 0);
 		bus_space_write_4(tag, ioh1, 0, 4);
+
 	}
 
-//cout <<  "Mapping bus space.  Number Of Words = " << numberOfWords << endl;
-//cin >> numberOfWords;
-//cout <<  "Address != 0x90000008 " << memoryAddress << endl;
-//cin >> memoryAddress;
-
-
-	if ( (ret = bus_space_map(tag, memoryAddress, 4 * numberOfWords, 0, &ioh)) )
+	if( tag != NULL && (memoryAddress != lastMemoryAddress || numberOfWords != lastNumberOfWords) )
 	{
-	//		errx(1, "could not map bus space, error %d", ret);
-		std::cerr << "Could not map bus space, error " << ret << std::endl;
-	}
+		std::cout << "Mapping new bus space" << std::endl;
 
-//cout << "success!" << endl;
+		//The tag exists and refers to a memory blcok that is different than the currently requested block
+		lastMemoryAddress = memoryAddress;
+		lastNumberOfWords = numberOfWords;
+
+		//Map a new memory block
+		if( (ret = bus_space_map(tag, memoryAddress, 4 * numberOfWords, 0, &ioh)) )
+		{
+			std::cerr << "Could not map bus space, error " << ret << std::endl;
+		}
+	}
 #endif
 
 }
@@ -90,24 +102,25 @@ void EtraxBus::writeDataToAddress(uInt32 data, uInt32 address)
 		offset = address - memoryAddress;
 	}
 	else
+	{
 		std::cout << "Error in EtraxBus::writeDataToAddress().  Address '" 
-		<< address << "' is below range '" << memoryAddress << "'." << endl;
+			<< address << "' is below range '" << memoryAddress << "'." << endl;
+	}
 
 	if(offset < 4 * numberOfWords)
 	{
 		writeData(data, offset);
 	}
 	else
+	{
 		std::cout << "Error in EtraxBus::writeDataToAddress().  Address '" 
-		<< address << "' is above range '" << (memoryAddress + 4 * numberOfWords) << "'." << endl;
+			<< address << "' is above range '" << (memoryAddress + 4 * numberOfWords) << "'." << endl;
+	}
 }
 
 void EtraxBus::writeData(uInt32 data, uInt32 addressOffset)
 {
 #ifdef HAVE_LIBBUS
-
-//cout << "Writing to offset: " << addressOffset << ". Mapped size = " << numberOfWords << endl;
-//cin >> addressOffset;
 	bus_space_write_4(tag, ioh, addressOffset, data);
 //	bus_space_barrier(tag, ioh, addressOffset, 4, BUS_SPACE_BARRIER_WRITE_BEFORE_WRITE);
 #endif
@@ -121,16 +134,21 @@ uInt32 EtraxBus::readDataFromAddress(uInt32 address)
 		offset = address - memoryAddress;
 	}
 	else
+	{
 		std::cout << "Error in EtraxBus::readDataFromAddress().  Address '" 
-		<< address << "' is below range '" << memoryAddress << "'." << endl;
+			<< address << "' is below range '" << memoryAddress << "'." << endl;
+	}
 
 	if(offset < 4 * numberOfWords)
 	{
 		return readData(offset);
 	}
 	else
+	{
 		std::cout << "Error in EtraxBus::readDataFromAddress().  Address '" 
-		<< address << "' is above range '" << (memoryAddress + 4 * numberOfWords) << "'." << endl;
+			<< address << "' is above range '" << (memoryAddress + 4 * numberOfWords) << "'." << endl;
+	}
+
 	return 0;
 }
 
@@ -138,9 +156,7 @@ uInt32 EtraxBus::readData(uInt32 addressOffset)
 {
 	uInt32 value = 0;
 
-	#ifdef HAVE_LIBBUS
-//cout << "Reading from offset: " << addressOffset << ". Mapped size = " << numberOfWords << endl;
-//cin >> addressOffset;
+#ifdef HAVE_LIBBUS
 
 	readMutex->lock();
 	{
@@ -150,7 +166,7 @@ uInt32 EtraxBus::readData(uInt32 addressOffset)
 
 		//bus_space_barrier(space, handle, offset, length, flags);
 //		bus_space_barrier(tag, ioh, addressOffset, 4, BUS_SPACE_BARRIER_READ_BEFORE_READ);
-	#endif
+#endif
 	
 	return value;
 }
@@ -158,9 +174,13 @@ uInt32 EtraxBus::readData(uInt32 addressOffset)
 
 void EtraxBus::setMemoryAddress(uInt32 MemoryAddress, uInt32 NumberOfWords)
 {
-//cout << "Memory address = " << MemoryAddress << endl;
+	//save old values to check if the new values are actually different
+	lastMemoryAddress = memoryAddress;
+	lastNumberOfWords = numberOfWords;
+
 	memoryAddress = MemoryAddress;
 	numberOfWords = NumberOfWords;
+	
 	setupMemoryBus();
 }
 
