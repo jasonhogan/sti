9b8039b8a03594ceb856665a2d162d5279af6da3
FPGA <hogan@stanford.edu>
2013-04-08 22:56:55 +0000

    Fixes to PhaseMatrix device list mode
    * Hard timing "play" without requiring a reparse.
    * Based on an initialization event that runs LIST:STAR 1
    
    Jason

--- devices/PhaseMatrix/src/main.cpp
+++ devices/PhaseMatrix/src/main.cpp
@@ -13,8 +13,11 @@ int main(int argc, char* argv[])
 	int comPortNumber1 = 3;
 	int comPortNumber2 = 4;
 
-	PhaseMatrixDevice phaseMatrix(orbManager, "PhaseMatrix", "EPMezzanine1.stanford.edu", 0, comPortNumber1);
+	PhaseMatrixDevice phaseMatrix1(orbManager, "PhaseMatrix", "EPMezzanine1.stanford.edu", 0, comPortNumber1);
 	PhaseMatrixDevice phaseMatrix2(orbManager, "PhaseMatrix 2", "EPMezzanine1.stanford.edu", 1, comPortNumber2);
+
+	phaseMatrix1.setSaveAttributesToFile(true);
+	phaseMatrix2.setSaveAttributesToFile(true);
 
 	orbManager->run();
 
