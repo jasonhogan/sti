9080fb295ad19afe1a498f9d7f09fb88ba154993
FPGA <FPGA@8bcac300-4d60-4aec-84ba-b7489f70e69b>
2013-08-09 00:18:57 +0000

    High power intensity lock executable now starts two locks. Sensor and actuator partner devices are given through the configuration files.
    -Susannah
    
    git-svn-id: https://atom-svn.stanford.edu/svn/FPGA/sti/trunk@1057 8bcac300-4d60-4aec-84ba-b7489f70e69b

--- devices/highPowerIntensityLock/src/main.cpp
+++ devices/highPowerIntensityLock/src/main.cpp
@@ -10,11 +10,17 @@ int main(int argc, char* argv[])
 {
 	orbManager = new ORBManager(argc, argv);    
 
-	HighPowerIntensityLockDevice highPowerIntensityLock1(orbManager, "High Power Intensity Lock 1", "EPMezzanine1.stanford.edu", 0);
+//	HighPowerIntensityLockDevice highPowerIntensityLock1(orbManager, "High Power Intensity Lock 1", "EPMezzanine1.stanford.edu", 0);
 //	HighPowerIntensityLockDevice highPowerIntensityLock2(orbManager, "High Power Intensity Lock 2", "EPMezzanine1.stanford.edu", 1);
 
+	string configFilename1 = "highPowerIntensityLock1.ini"; //default
+	string configFilename2 = "highPowerIntensityLock2.ini"; //default
+
+	HighPowerIntensityLockDevice highPowerIntensityLock1(orbManager, "High Power Intensity Lock 1", configFilename1);
+	HighPowerIntensityLockDevice highPowerIntensityLock2(orbManager, "High Power Intensity Lock 2", configFilename2);
+
 	highPowerIntensityLock1.setSaveAttributesToFile(true);
-//	highPowerIntensityLock2.setSaveAttributesToFile(true);
+	highPowerIntensityLock2.setSaveAttributesToFile(true);
 
 	orbManager->run();
 
