9873818cebe8f4def3aed485f5f34219dc422eec
hogan <hogan@8bcac300-4d60-4aec-84ba-b7489f70e69b>
2014-05-14 06:09:13 +0000

    Cleaning up some Server files
    * Cleaned up print statements for registering/removing devices on the Server.
    * Removing some very old commented sections
    
    Jason
    
    git-svn-id: https://atom-svn.stanford.edu/svn/FPGA/sti/trunk@1132 8bcac300-4d60-4aec-84ba-b7489f70e69b

--- server/src/autoserver.cpp
+++ server/src/autoserver.cpp
@@ -1,38 +1,15 @@
 /*! \file autoserver.cpp
  *  \author Jason Hogan
- *  \brief A test server which automatically mounts new STI_Devices.
+ *  \brief main() for the STI Server
  */
-
-
-
-
-
-#include <omniORB4/omniURI.h>
-#include <string>
-#include <iostream>
 
 #include <ORBManager.h>
 #include "corba/STI_Server.h"
 
-#define _CRTDBG_MAP_ALLOC
-#include <stdlib.h>
-#include <crtdbg.h>
-
-using namespace std;
-
-ORBManager* orbManager;
-STI_Server* server;
-
 int main(int argc, char **argv)
 {
-	_CrtDumpMemoryLeaks();
-
-	orbManager = new ORBManager(argc, argv);
-	server = new STI_Server("STI Server", orbManager);
-
-
-
-//	cerr << orbManager->errMsg() << endl;
+	ORBManager* orbManager = new ORBManager(argc, argv);
+	STI_Server* server = new STI_Server("STI Server", orbManager);
 
 	orbManager->run();
 	
