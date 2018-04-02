88fbe7555bd5b4ade48d6c9f8e0a647620f30381
bklopfer <bklopfer@hingadinga.stanford.edu>
2018-04-02 13:25:27 -0700

    Translating run_idlj.bat to run_idlj.sh

---
+++ client/nbproject/run_idlj.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd ../../idl
+
+idlj -fall -emitAll -pkgTranslate STI edu.stanford.atom.sti.corba -pkgTranslate Client_Server edu.stanford.atom.sti.corba.Client_Server -pkgTranslate Types edu.stanford.atom.sti.corba.Types -td ./../client/src ./orbTypes.idl
+idlj -fall -emitAll -pkgTranslate STI edu.stanford.atom.sti.corba -pkgTranslate Client_Server edu.stanford.atom.sti.corba.Client_Server -pkgTranslate Types edu.stanford.atom.sti.corba.Types -td ./../client/src ./client.idl
+idlj -emitAll -pkgTranslate Remote_File_Server edu.stanford.atom.sti.RemoteFileServer.comm.corba -td ./../client/src ./FileServer.idl
+idlj -fall -emitAll -pkgTranslate STI edu.stanford.atom.sti.corba -pkgTranslate Pusher edu.stanford.atom.sti.corba.Pusher -td ../client/src ./pusher.idl
+
